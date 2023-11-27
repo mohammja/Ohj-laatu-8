551dfeee93f012fb913774a2fcb0490b6d950a0a
Mohammed Al-Jewari <mjassim18@gmail.com>
2023-11-27 20:41:42 +0200

    All done!

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int aika = 86399;
+        String tulos = TimeUtils.secToTime(aika);
+        System.out.println("Aika: " + tulos);
+
+    }
+}
